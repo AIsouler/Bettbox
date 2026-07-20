0c316372dfd80952a62439e9bf0945626a1bb7ab
AIsouler <zy43220@gmail.com>
2026-07-20 17:44:45 +0800

    fix

--- lib/manager/app_manager.dart
+++ lib/manager/app_manager.dart
@@ -162,6 +162,7 @@ class _AppStateManagerState extends ConsumerState<AppStateManager>
       globalState.appController.savePreferences();
       await globalState.handleBackground();
     } else if (state == AppLifecycleState.resumed) {
+      globalState.markResumed();
       globalState.handleForeground();
       render?.resume();
       await globalState.resumeForegroundUpdates();
